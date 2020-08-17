8d80a5fe1d473474876adae728aa0b8b34853bd7
Seth <sethgoldin27@gmail.com>
2020-08-17 16:55:49 -0400

    swap to single channel

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn api_server:app --log-file -
 release: python3 utils/init_db.py
-worker: python3 start_scraper.py
+worker: python3 start_api.py
--- api_server/__init__.py
+++ api_server/__init__.py
@@ -5,11 +5,11 @@ from flask import Flask
 from flask_cors import CORS
 import googleapiclient.discovery
 
-__all__ = ["cur", "app", "mc", "youtube", "images_list", "YOUTUBE_CHANNELS"]
+__all__ = ["cur", "app", "mc", "youtube", "images_list", "YOUTUBE_CHANNEL"]
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
-YOUTUBE_CHANNELS = os.environ["YOUTUBE_CHANNEL_IDS"].split(";")
+YOUTUBE_CHANNEL = os.environ["YOUTUBE_CHANNEL_ID"]
 
 conn = psycopg2.connect(DATABASE_URL)  # , sslmode="require")
 cur = conn.cursor()
