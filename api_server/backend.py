ec047d36954cc618198da2f82e2f6a07e040031c
Seth <sethgoldin27@gmail.com>
2020-08-17 18:16:01 -0400

    add gameplay and popular

--- api_server/backend.py
+++ api_server/backend.py
@@ -9,7 +9,7 @@ import googleapiclient.errors
 from . import mc, youtube, YOUTUBE_CHANNEL, YOUTUBE_API_KEY
 
 # TODO: is it possible to remove these globals?
-latest_response = {}
+youtube_response = {}
 # gameplay_response = {}
 # popular_response = {}
 
@@ -41,9 +41,9 @@ def getPlaylists():
 
 
 def cb(c, request_id, response, exception):
-    if latest_response.get(c) is None:
-        latest_response[c] = []
-    latest_response[c].append(response)
+    if youtube_response.get(c) is None:
+        youtube_response[c] = []
+    youtube_response[c].append(response)
 
 
 def fetch_youtube_latest_uploads(page_token=None):
@@ -68,82 +68,87 @@ def fetch_youtube_latest_uploads(page_token=None):
 
     data = {
         "kind": "youtube",
-        "nextPageToken": latest_response[this_counter][0]["nextPageToken"],
+        "nextPageToken": youtube_response[this_counter][0]["nextPageToken"],
         "items": list(
             itertools.chain.from_iterable(
                 map(lambda x: x["items"],
-                    latest_response[this_counter])
+                    youtube_response[this_counter])
             )
         ),
     }
     mc.add("LATEST_PAGE_" + str(page_token), json.dumps(data), time=60 * 30)
-    del latest_response[this_counter]
+    del youtube_response[this_counter]
 
     return data
 
-#
-# def fetch_youtube_gameplay(page_token=None):
-#     gameplay_page_cached = mc.get("GAMEPLAY_PAGE_" + str(page_token))
-#     if gameplay_page_cached is not None:
-#         return json.loads(gameplay_page_cached)
-#
-#     global global_counter
-#
-#     this_counter = global_counter
-#     global_counter += 1
-#
-#     batch = youtube.new_batch_http_request()
-#     batch.add(
-#         youtube.playlistItems().list(
-#             part="snippet", playlistId='PL0VGXNIrhFXhZy_AAbsnKyZdRFJmabKzQ', maxResults=15, pageToken=page_token
-#         ),
-#         callback=functools.partial(cb, this_counter),
-#     )
-#
-#     data = {
-#         "kind": "youtube",
-#         "nextPageToken": gameplay_response[this_counter][0]["nextPageToken"],
-#         "items": list(
-#             itertools.chain.from_iterable(
-#                 map(lambda x: x["items"],
-#                     gameplay_response[this_counter])
-#             )
-#         ),
-#     }
-#     mc.add("GAMEPLAY_PAGE_" + str(page_token), json.dumps(data), time=60 * 30)
-#     del gameplay_response[this_counter]
-#
-#     return data
-#
-#
-# def fetch_youtube_popular(page_token=None):
-#     popular_page_cached = mc.get("POPULAR_PAGE_" + str(page_token))
-#     if popular_page_cached is not None:
-#         return json.loads(popular_page_cached)
-#
-#     global global_counter
-#
-#     this_counter = global_counter
-#     global_counter += 1
-#
-#     batch = youtube.new_batch_http_request()
-#     batch.add(
-#         youtube.playlistItems().list(
-#             part="snippet", playlistId='', maxResults=2, pageToken=page_token
-#         ),
-#         callback=functools.partial(cb, this_counter),
-#     )
-#     data = {
-#         "kind": "youtube",
-#         "nextPageToken": global_youtube_response[this_counter][0]["nextPageToken"],
-#         "items": list(
-#             itertools.chain.from_iterable(
-#                 map(lambda x: x["items"],
-#                     global_youtube_response[this_counter])
-#             )
-#         ),
-#     }
-#     mc.add("POPULAR_PAGE_" + str(page_token), json.dumps(data), time=60 * 30)
-#     del global_youtube_response[this_counter]
-#
-#     return data
+
+def fetch_youtube_gameplay(page_token=None):
+    youtube_page_cached = mc.get("GAMEPLAY_PAGE_" + str(page_token))
+    if youtube_page_cached is not None:
+        return json.loads(youtube_page_cached)
+
+    global global_counter
+
+    this_counter = global_counter
+    global_counter += 1
+
+    batch = youtube.new_batch_http_request()
+    batch.add(
+        youtube.playlistItems().list(
+            part="snippet", playlistId='PL0VGXNIrhFXhZy_AAbsnKyZdRFJmabKzQ', maxResults=15, pageToken=page_token
+        ),
+        callback=functools.partial(cb, this_counter),
+    )
+    batch.execute()
+
+    data = {
+        "kind": "youtube",
+        "nextPageToken": youtube_response[this_counter][0]["nextPageToken"],
+        "items": list(
+            itertools.chain.from_iterable(
+                map(lambda x: x["items"],
+                    youtube_response[this_counter])
+            )
+        ),
+    }
+    mc.add("GAMEPLAY_PAGE_" + str(page_token), json.dumps(data), time=60 * 30)
+    del youtube_response[this_counter]
+
+    return data
+
+def fetch_youtube_popular(page_token=None):
+    youtube_page_cached = mc.get("POPULAR_PAGE_" + str(page_token))
+    if youtube_page_cached is not None:
+        return json.loads(youtube_page_cached)
+
+    global global_counter
+
+    this_counter = global_counter
+    global_counter += 1
+
+    batch = youtube.new_batch_http_request()
+    batch.add(
+        request = youtube.search().list(
+            part="snippet",
+            channelId="UCPqT2ULat4WIzWKqpAAOlIQ",
+            maxResults=10,
+            order="viewCount"
+        ),
+        callback=functools.partial(cb, this_counter)
+    )
+    batch.execute()
+
+    data = {
+        "kind": "youtube",
+        "nextPageToken": youtube_response[this_counter][0]["nextPageToken"],
+        "items": list(
+            itertools.chain.from_iterable(
+                map(lambda x: x["items"],
+                    youtube_response[this_counter])
+            )
+        ),
+    }
+    mc.add("POPULAR_PAGE_" + str(page_token), json.dumps(data), time=60 * 30)
+    del youtube_response[this_counter]
+
+    return data
--- api_server/routes.py
+++ api_server/routes.py
@@ -10,16 +10,16 @@ def videos():
     return json.dumps(backend.fetch_youtube_latest_uploads(page_token))
 
 
-# @app.route("/gameplay")
-# def gameplay():
-#     page_token = request.args.get("page_token")
-#     return json.dumps(backend.fetch_youtube_gameplay(page_token))
-#
-#
-# @app.route("/popular")
-# def popular_videos():
-#     page_token = request.args.get("page_token")
-#     return json.dumps(backend.fetch_youtube_popular(page_token))
+@app.route("/gameplay")
+def gameplay():
+    page_token = request.args.get("page_token")
+    return json.dumps(backend.fetch_youtube_gameplay(page_token))
+
+
+@app.route("/popular")
+def popular_videos():
+    page_token = request.args.get("page_token")
+    return json.dumps(backend.fetch_youtube_popular(page_token))
 
 
 
